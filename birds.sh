e1be74334b1ecf476e24abf9433b95ff3bea6465
Tim Jose <tjose@Tims-MacBook-Pro.local>
2017-03-17 17:59:54 -0700

    I believe I can fly

--- birds.sh
+++ birds.sh
@@ -18,6 +18,8 @@ filename=
 yn=
 
 usage="Usage: birds.sh project_directory_name"
+noinput="Nothing entered, using project's name instead."
+html_init="<!DOCTYPE html>\n<html>\n<body>\n\n<h1></h1>\n<p></p>\n\n</body>\n</html>"
 
 git_ignore ()
 {
@@ -43,15 +45,20 @@ echo "Select a language by entering a number:\n1. C\n2. C++\n3. SHELL\n4. PYTHON
 read language
 if [ $language = 1 ]
 then
-	echo "Enter file name:"
+	echo "You chose C.\nEnter file name:"
 	read filename
-	if [ 
 	touch ../$project/Makefile
 	mkdir ../$project/srcs
-	touch ../$project/srcs/$filename.c
 	mkdir ../$project/includes
 	git_ignore
-	echo "Do you want libft? [y / n]"
+	if [ -z $filename ]
+	then
+		echo $noinput
+		touch ../$project/srcs/$project.c
+	else
+		touch ../$project/srcs/$filename.c
+	fi
+	echo "Do you want to use libft? [y / n]"
 	read yn
 	if [ $yn = "y" ] || [ $yn = "Y" ]
 	then
@@ -60,8 +67,69 @@ then
 	then
 		echo "I didn't understand that.. I'll just assume \"no\""
 	fi
+elif [ $language = 2 ]
+then
+	echo "You chose C++.\nEnter file name:"
+	read filename
+	git_ignore
+	if [ -z $filename ]
+	then
+		echo $noinput
+		touch ../$project/$project.cpp
+	else
+		touch ../$project/$filename.cpp
+	fi
+elif [ $language = 3 ]
+then
+	echo "You chose SHELL.\nEnter file name:"
+	read filename
+	git_ignore
+	if [ -z $filename ]
+	then
+		echo $noinput
+		touch ../$project/$project.sh
+	else
+		touch ../$project/$filename.sh
+	fi
+elif [ $language = 4 ]
+then
+	echo "You chose PYTHON.\nEnter file name:"
+	read filename
+	git_ignore
+	if [ -z $filename ]
+	then
+		echo $noinput
+		touch ../$project/$project.py
+	else
+		touch ../$project/$filename.py
+	fi
+elif [ $language = 5 ]
+then
+	echo "You chose RUBY.\nEnter file name:"
+	read filename
+	git_ignore
+	if [ -z $filename ]
+	then
+		echo $noinput
+		touch ../$project/$project.rb
+	else
+		touch ../$project/$filename.rb
+	fi
+elif [ $language = 6 ]
+then
+	echo "You chose HTML.\nEnter file name:"
+	read filename
+	git_ignore
+	if [ -z $filename ]
+	then
+		echo $noinput
+		touch ../$project/$project.html
+		echo $html_init >> ../$project/$project.html
+	else
+		touch ../$project/$filename.html
+		echo $html_init >> ../$project/$filename.html
+	fi
 else
-	echo "good job"
+	echo "Not a valid number"
+	rmdir ../$project
 fi
-
-
